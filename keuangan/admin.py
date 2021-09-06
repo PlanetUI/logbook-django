4d19d0fa0ca5643f4e2b9a763f4b68dcf9c19496
Mashanz <mangatkk@gmail.com>
2021-09-06 20:21:56 +0700

    [UPD] fields admin

--- keuangan/admin.py
+++ keuangan/admin.py
@@ -5,9 +5,9 @@ from django.contrib.admin import DateFieldListFilter
 from .models import CashFlow, CashFlowCategory
 
 class CashFlowAdmin(admin.ModelAdmin):
-    list_display = ['tanggal', 'nama_item', 'qty', 'harga_satuan', 'harga_total']
-    search_fields = ['tanggal', 'nama_item', 'qty', 'harga_satuan', 'harga_total']
-    list_filter = ['tanggal', 'nama_item']
+    list_display = ['tanggal', 'kategory', 'nama_item', 'qty', 'harga_satuan', 'harga_total']
+    search_fields = ['tanggal', 'kategory', 'nama_item', 'qty', 'harga_satuan', 'harga_total']
+    list_filter = ['tanggal', 'kategory', 'nama_item']
 
 admin.site.register(CashFlow, CashFlowAdmin)
 admin.site.register(CashFlowCategory)
