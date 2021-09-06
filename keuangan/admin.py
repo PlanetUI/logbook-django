8ef397f806bceeaa8033198310e890ace0e690a8
Mashanz <mangatkk@gmail.com>
2021-09-06 19:47:13 +0700

    [UPD] models cashflow

--- keuangan/admin.py
+++ keuangan/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.admin import DateFieldListFilter
 
 # Register your models here.
+from .models import CashFlow
+
+class CashFlowAdmin(admin.ModelAdmin):
+    list_display = ['tanggal', 'nama_item', 'qty', 'harga_satuan', 'harga_total']
+    search_fields = ['tanggal', 'nama_item', 'qty', 'harga_satuan', 'harga_total']
+    list_filter = ['tanggal', 'nama_item']
+
+admin.site.register(CashFlow, CashFlowAdmin)
--- keuangan/models.py
+++ keuangan/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class CashFlow(models.Model):
+    tanggal = models.DateTimeField('Tanggal Transaksi')
+    nama_item = models.CharField(max_length=200)
+    qty = models.IntegerField(default=1)
+    harga_satuan = models.FloatField(default=0.0)
+    harga_total = models.FloatField(default=0.0)
+
+    def __str__(self) -> str:
+        return self.nama_item
