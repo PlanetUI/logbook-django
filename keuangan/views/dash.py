4b96fe12f1885bfc9e882a78937352515ba5d3fc
Mashanz <mangatkk@gmail.com>
2021-09-11 21:53:47 +0700

    [ADD] dashboard

--- keuangan/views/dash.py
+++ keuangan/views/dash.py
@@ -2,5 +2,13 @@ from django.shortcuts import render, redirect
 
 def dash(request):
     html_template = 'keuangan/dash.html'
-    context = {}
+
+    getyear = int(request.GET.get('tahun', 2021))
+
+    context = {
+        'prevYear': getyear - 1,
+        'thisYear': getyear,
+        'nextYear': getyear + 1
+
+    }
     return render(request, html_template, context)
