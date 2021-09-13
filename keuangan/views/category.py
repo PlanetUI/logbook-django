b6f55db5850f2cd8c02ffcf5e9e93a379166e3bc
Mashanz <mangatkk@gmail.com>
2021-09-13 22:45:06 +0700

    [ADD] no data images

--- keuangan/views/category.py
+++ keuangan/views/category.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
+from ..models import CashFlowCategory
 
 def category(request):
     html_template = 'keuangan/category.html'
-    context = {}
+
+    cashFlowCategory = CashFlowCategory.objects.all().order_by('-pk')
+
+    context = {'cashFlowCategory': cashFlowCategory}
     return render(request, html_template, context)
--- keuangan/views/product.py
+++ keuangan/views/product.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
+from ..models import Product
+
 
 def product(request):
     html_template = 'keuangan/product.html'
-    context = {}
+
+    product = Product.objects.all().order_by('-pk')
+
+    context = {'product': product}
     return render(request, html_template, context)
