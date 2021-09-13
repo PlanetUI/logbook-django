126ca9ba5be5ce68130c3e9318273143084f5ef2
Mashanz <mangatkk@gmail.com>
2021-09-13 21:26:58 +0700

    [UPD] get details objects

--- keuangan/views/pengeluaran_form.py
+++ keuangan/views/pengeluaran_form.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render, redirect
+from ..models import CashFlow
 
 def pengeluaran_form(request):
     html_template = 'keuangan/pengeluaran_form.html'
-    context = {}
+
+    getYear = int(request.GET.get('tahun', 2021))
+    getMonth = int(request.GET.get('bulan', 1))
+
+    object_id = int(request.GET.get('id', 0))
+    record = False
+    if object_id != 0:
+        record = CashFlow.objects.get(pk=object_id)
+        record.tanggal = str(record.tanggal)[0:10]
+
+    context = {
+        'record': record,
+        'thisYear': getYear,
+        'thisMonth': getMonth,
+    }
     return render(request, html_template, context)
