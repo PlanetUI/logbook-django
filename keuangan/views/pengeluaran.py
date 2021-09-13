b7db00b96737100eccb997fe8676cc742168e754
Mashanz <mangatkk@gmail.com>
2021-09-13 21:12:18 +0700

    [UPD] graph view

--- keuangan/views/pengeluaran.py
+++ keuangan/views/pengeluaran.py
@@ -1,6 +1,89 @@
 from django.shortcuts import render, redirect
+from ..models import CashFlow
+
+
+def monthString(number=1):
+    data = {
+        1: 'Januari',
+        2: 'February',
+        3: 'Maret',
+        4: 'April',
+        5: 'Mei',
+        6: 'Juni',
+        7: 'Juli',
+        8: 'Agustus',
+        9: 'September',
+        10: 'Oktober',
+        11: 'November',
+        12: 'Desember',
+    }
+    return data[number]
+
+def getCashFlow(tahun=2021, bulan=1):
+    cashFlow = CashFlow.objects.all().order_by('-pk').filter(tanggal__year = tahun, tanggal__month = bulan)
+
+    total = 0
+    list_kategori = []
+    total_kategori = []
+    list_product = []
+    total_product = []
+    for cash in cashFlow:
+        total += cash.harga_total
+        if str(cash.kategory) not in list_kategori:
+            list_kategori.append(str(cash.kategory))
+            total_kategori.append(0)
+        if str(cash.produk) not in list_product:
+            list_product.append(str(cash.produk))
+            total_product.append(0)
+
+    for key, value in enumerate(list_kategori):
+        for cash in cashFlow:
+            if value == str(cash.kategory):
+                total_kategori[key] += cash.harga_total
+            if value == str(cash.kategory):
+                total_product[key] += cash.harga_total
+    return {
+        'total': total,
+        'cashFlow': cashFlow,
+        'list_kategori': list_kategori,
+        'total_kategori': total_kategori,
+        'list_product': list_product,
+        'total_product': total_product,
+
+    }
 
 def pengeluaran(request):
     html_template = 'keuangan/pengeluaran.html'
-    context = {}
+
+    getYear = int(request.GET.get('tahun', 2021))
+    getMonth = int(request.GET.get('bulan', 1))
+    getView = str(request.GET.get('view', 'tree'))
+
+    if getMonth == 1:
+        prevYear = getYear - 1
+        prevMonth = 12
+        nextYear = getYear
+        nextMonth = getMonth + 1
+    elif getMonth == 12:
+        prevYear = getYear
+        prevMonth = getMonth - 1
+        nextYear = getYear + 1
+        nextMonth = 1
+    else:
+        prevYear = getYear
+        prevMonth = getMonth - 1
+        nextYear = getYear
+        nextMonth = getMonth + 1
+
+    context = {
+        'thisYear': getYear,
+        'thisMonth': getMonth,
+        'thisMonthString': monthString(getMonth),
+        'prevYear': prevYear,
+        'prevMonth': prevMonth,
+        'nextYear': nextYear,
+        'nextMonth': nextMonth,
+        'getView': getView,
+        'data': getCashFlow(getYear, getMonth)
+    }
     return render(request, html_template, context)
